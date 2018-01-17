cdceb3d26b6b1d7e14d7eb123abe48aa253ecf49
blopa <montenegro.pablo@outlook.com>
2018-01-17 21:53:24 -0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+var SRC_DIR = path.resolve(__dirname, 'src');
+var DIST_DIR = path.resolve(__dirname, 'dist');
+
+var config = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-2']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
