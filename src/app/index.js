7498dae4f2aabac320f22ffa0542438b8ba624d1
blopa <montenegro.pablo@outlook.com>
2018-01-17 21:59:24 -0200

    creating first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world from Resume Builder');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Hello World from Resume Builder!
+                </h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById('app'));
