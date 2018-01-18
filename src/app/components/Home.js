c3352bcfece73de9765dadb4c68fed1d74ae455b
blopa <montenegro.pablo@outlook.com>
2018-01-17 22:37:39 -0200

    adding multiple components

---
+++ src/app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>Home component</p>
+            </div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Header } from './components/Header'
+import { Home } from './components/Home'
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Hello World from Resume Builder!
-                </h1>
+            <div className="container">
+                <div className="row">
+                    <Header/>
+                </div>
+                <div className="row">
+                    <Home/>
+                </div>
             </div>
         );
     }
