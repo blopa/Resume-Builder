24085b6c45318f7b9552257183f2571b07f91d6a
blopa <montenegro.pablo@outlook.com>
2018-01-19 19:13:22 -0200

    add new components

---
+++ src/app/components/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Resume extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>Spreadsheet Parser</p>
+            </div>
+        );
+    }
+}
+
+Resume.propTypes = {
+    // https://reactjs.org/docs/typechecking-with-proptypes.html
+};
---
+++ src/app/components/SpreadsheetParser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class SpreadsheetParser extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>Spreadsheet Parser</p>
+            </div>
+        );
+    }
+}
+
+SpreadsheetParser.propTypes = {
+    // https://reactjs.org/docs/typechecking-with-proptypes.html
+};
