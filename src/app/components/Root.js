3d994a3492a80aeac7c7532ad2cfeb20c1672251
blopa <montenegro.pablo@outlook.com>
2018-01-23 12:30:21 -0200

    using hashrouter

--- src/app/components/Root.js
+++ src/app/components/Root.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Header } from './Header'
+import {Home} from "./Home";
+import {SpreadsheetParser} from "./SpreadsheetParser";
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 export class Root extends React.Component {
     render() {
         return(
-            <div className="container">
-                <div className="row">
-                    <Header/>
-                </div>
-                <div className="row">
-                    <p>Nothing here</p>
-                    {this.props.children}
-                </div>
-            </div>
+            <Router>
+                <Switch>
+                    <div className="container">
+                        <div className="row">
+                            <Header/>
+                        </div>
+                        <div className="row">
+                            <Route path={'home'} component={Home}/>
+                            <Route path={'spreadsheetparser'} component={SpreadsheetParser}/>
+                        </div>
+                    </div>
+                </Switch>
+            </Router>
         );
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Root} from './components/Root'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Home} from './components/Home';
-import {SpreadsheetParser} from './components/SpreadsheetParser';
 
 class App extends React.Component {
     render() {
         return (
-            <Router>
-                <Route path={'/'} component={Root}>
-                    <Route path={'home'} component={Home}/>
-                    <Route path={'spreadsheetparser'} component={SpreadsheetParser}/>
-                </Route>
-            </Router>
+            <Root/>
         );
     }
 }
