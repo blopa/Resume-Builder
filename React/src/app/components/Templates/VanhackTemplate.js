0b74a3cc8fb77be623adb77fc6f91a4811b05f90
blopa <montenegro.pablo@outlook.com>
2018-02-11 18:57:46 -0200

    validate display to display informations

--- React/src/app/components/Templates/VanhackTemplate.js
+++ React/src/app/components/Templates/VanhackTemplate.js
@@ -26,39 +26,44 @@ export class VanhackTemplate extends React.Component {
                         <p id="resume-github"><a href={resume.github.content} target="_blank">{resume.github.content}</a></p>
                     </div>
                 </div>
-                <div id="work-experience">
-                    {resume.experience.content.length > 0 ? <h4>WORK EXPERIENCE</h4> : null}
-                    <ul className="resume-list">
-                        {resume.experience.content.map(function(value, key){
-                            return (
-                                <li key={key}>
-                                    <p className="resume-job-title-company">{value.jobTitle.content}, {value.company.content}</p>
-                                    <p className="resume-local-time">{value.from.content} - {value.to.content} - {value.local.content}</p>
-                                    <ul className="resume-list">
-                                        {value.items.content.map(function (value, key) {
-                                            return <li key={key} className="resume-job-achievements">{value.content}</li>
-                                        })}
-                                    </ul>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-                <div id="side-projects">
-                    {resume.sideProject.content.length > 0 ? <h4>SIDE PROJECTS</h4> : null}
-                    <ul className="resume-list">
-                        {resume.sideProject.content.map(function(value, key){
-                            return (
-                                <li key={key} className="resume-side-job">
-                                    <p><a href={value.url.content} target="_blank">{value.projectName.content}</a>: {value.description.content}</p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-                <div id="education">
-                    {resume.education.content.length > 0 ? <h4>EDUCATION</h4> : null}
-                    <ul className="resume-list">
+                {resume.experience.display ? (
+                    <div id="work-experience">
+                        {resume.experience.content.length > 0 ? <h4>WORK EXPERIENCE</h4> : null}
+                        <ul className="resume-list">
+                            {resume.experience.content.map(function(value, key){
+                                return (
+                                    <li key={key}>
+                                        <p className="resume-job-title-company">{value.jobTitle.content}, {value.company.content}</p>
+                                        <p className="resume-local-time">{value.from.content} - {value.to.content} - {value.local.content}</p>
+                                        <ul className="resume-list">
+                                            {value.items.content.map(function (value, key) {
+                                                return <li key={key} className="resume-job-achievements">{value.content}</li>
+                                            })}
+                                        </ul>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                ) : null}
+                {resume.sideProject.display ? (
+                    <div id="side-projects">
+                        {resume.sideProject.content.length > 0 ? <h4>SIDE PROJECTS</h4> : null}
+                        <ul className="resume-list">
+                            {resume.sideProject.content.map(function(value, key){
+                                return (
+                                    <li key={key} className="resume-side-job">
+                                        <p><a href={value.url.content} target="_blank">{value.projectName.content}</a>: {value.description.content}</p>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                ) : null}
+                {resume.education.display ? (
+                    <div id="education">
+                        {resume.education.content.length > 0 ? <h4>EDUCATION</h4> : null}
+                        <ul className="resume-list">
                         {resume.education.content.map(function(value, key){
                             return (
                                 <li key={key}>
@@ -71,16 +76,21 @@ export class VanhackTemplate extends React.Component {
                                 </li>
                             );
                         })}
-                    </ul>
-                </div>
-                <div id="skills">
-                    {resume.skills.content ? <h4>TECHNICAL SKILLS</h4> : null}
-                    <p>{resume.skills.content}</p>
-                </div>
-                <div id="languages">
-                    {resume.languages.content ? <h4>LANGUAGES</h4> : null}
-                    <p>{resume.languages.content}</p>
-                </div>
+                        </ul>
+                    </div>
+                    ) : null}
+                {resume.skills.display ? (
+                    <div id="skills">
+                        {resume.skills.content ? <h4>TECHNICAL SKILLS</h4> : null}
+                        <p>{resume.skills.content}</p>
+                    </div>
+                ) : null}
+                {resume.languages.display ? (
+                    <div id="languages">
+                        {resume.languages.content ? <h4>LANGUAGES</h4> : null}
+                        <p>{resume.languages.content}</p>
+                    </div>
+                ) : null}
             </div>
         );
     }
