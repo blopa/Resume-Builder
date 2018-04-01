e778a2f3e513ee6cbb073d95bd885be35986c044
blopa <montenegro.pablo@outlook.com>
2018-04-01 17:14:14 -0300

    changes on webpack settings

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SRC_DIR = path.resolve(__dirname, 'src');
 const DIST_DIR = path.resolve(__dirname, 'dist');
+const PUBLIC_PATH = '';
 
 const config = {
   entry: `${SRC_DIR}/app/index.js`,
   output: {
     path: `${DIST_DIR}/app`,
     filename: 'bundle.js',
-    publicPath: '/resume_builder/app/'
+    publicPath: `${PUBLIC_PATH}app/`
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Resume Builder',
+      favicon: `${SRC_DIR}/app/favicon.ico`,
+      publicPath: `${PUBLIC_PATH}app/`,
+      template: `${SRC_DIR}/template.html`,
+      filename: `${SRC_DIR}/index.html` // relative to root of the application
+    })
+  ],
   module: {
     rules: [
       {
