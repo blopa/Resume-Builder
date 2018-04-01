a7ad74aa6e0f63cb2e251d0dd97a8087b047f053
blopa <montenegro.pablo@outlook.com>
2018-04-01 17:32:03 -0300

    add condition to webpack config

--- React/webpack.config.js
+++ React/webpack.config.js
@@ -1,48 +1,56 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SRC_DIR = path.resolve(__dirname, 'src');
-const DIST_DIR = path.resolve(__dirname, 'dist');
-const PUBLIC_PATH = '';
 
-const config = {
-  entry: `${SRC_DIR}/app/index.js`,
-  output: {
-    path: `${DIST_DIR}/app`,
-    filename: 'bundle.js',
-    publicPath: `${PUBLIC_PATH}app/`
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: 'Resume Builder',
-      favicon: `${SRC_DIR}/app/favicon.ico`,
-      publicPath: `${PUBLIC_PATH}app/`,
-      template: `${SRC_DIR}/template.html`,
-      filename: `${SRC_DIR}/index.html` // relative to root of the application
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2']
-        }
-      },
-      {
-        test: /\.css$/,
-        include: SRC_DIR,
-        loaders: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|ico|gif)$/,
-        include: SRC_DIR,
-        loader: 'file-loader'
-      }
-    ]
+module.exports = function (env, argv) {
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const SRC_DIR = path.resolve(__dirname, 'src');
+  const DIST_DIR = path.resolve(__dirname, 'dist');
+  const PUBLIC_PATH = '';
+  let HTML_PATH = '';
+
+  if (argv.mode === 'production') {
+    HTML_PATH = DIST_DIR;
+  } else {
+    HTML_PATH = SRC_DIR;
   }
-};
 
-module.exports = config;
+  return {
+    entry: `${SRC_DIR}/app/index.js`,
+    output: {
+      path: `${DIST_DIR}/app`,
+      filename: 'bundle.js',
+      publicPath: `${PUBLIC_PATH}app/`
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        title: 'Resume Builder',
+        favicon: `${SRC_DIR}/app/favicon.ico`,
+        publicPath: `${PUBLIC_PATH}app/`,
+        template: `${SRC_DIR}/template.html`,
+        filename: `${HTML_PATH}/index.html` // relative to root of the application
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js?/,
+          include: SRC_DIR,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015', 'stage-2']
+          }
+        },
+        {
+          test: /\.css$/,
+          include: SRC_DIR,
+          loaders: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpg|ico|gif)$/,
+          include: SRC_DIR,
+          loader: 'file-loader'
+        }
+      ]
+    }
+  };
+};
