7f310ad94d9f37aa962e858ddfe6cec00f87872f
Pablo Benmaman <montenegro.pablo@outlook.com>
2019-08-24 12:56:53 +0200

    bump versions and add drawer

--- src/components/routes/BuildPage.jsx
+++ src/components/routes/BuildPage.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 // Components
 import DefaultTemplate from '../ui/Resume/Templates/Default/Default';
+import FloatingButton from '../ui/FloatingButton/FloatingButton';
+import Drawer from '../ui/Drawer/Drawer';
 
 // Utils
 import { getPropsFromLocationHistory, isObjectEmpty } from '../../utils/utils';
 
-export default function BuildPage(props) {
-    const { history } = props;
-    const { jsonResume } = getPropsFromLocationHistory(props);
+class BuildPage extends Component {
+    state = {
+        drawerOpen: false,
+    };
 
-    if (!jsonResume || isObjectEmpty(jsonResume)) {
-        // if no resume, then return to home
-        history.push('/upload');
-        return null;
+    toggleDrawer = () => {
+        this.setState((prevState) => ({
+            drawerOpen: !prevState.drawerOpen,
+        }));
+    };
+
+    render() {
+        const { history } = this.props;
+        const { jsonResume } = getPropsFromLocationHistory(this.props);
+
+        if (!jsonResume || isObjectEmpty(jsonResume)) {
+            // if no resume, then return to home
+            history.push('/upload');
+            return null;
+        }
+        console.log(jsonResume);
+
+        return (
+            <Fragment>
+                <FloatingButton
+                    onClick={this.toggleDrawer}
+                />
+                <Drawer
+                    open={this.state.drawerOpen}
+                    onClose={this.toggleDrawer}
+                >
+                    <ul>
+                        <li>Item 1</li>
+                        <li>Item 2</li>
+                        <li>Item 3</li>
+                    </ul>
+                </Drawer>
+                <DefaultTemplate
+                    resume={jsonResume}
+                />
+            </Fragment>
+        );
     }
-    console.log(jsonResume);
-
-    return (
-        <DefaultTemplate
-            resume={jsonResume}
-        />
-    );
 }
+
+export default BuildPage;
