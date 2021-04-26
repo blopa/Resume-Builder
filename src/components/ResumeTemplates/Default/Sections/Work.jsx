82e0287b4c7589ba81870a300faf4b0f25ff0e6d
Pablo Benmaman <montenegro.pablo@outlook.com>
2021-04-27 00:59:06 +0200

    improve the layout a bit

--- src/components/ResumeTemplates/Default/Sections/Work.jsx
+++ src/components/ResumeTemplates/Default/Sections/Work.jsx
@@ -3,25 +3,41 @@ import { v4 as uuid } from 'uuid';
 import { makeStyles } from '@material-ui/core/styles';
 import { IntlContext } from 'gatsby-plugin-intl';
 
-const useStyles = makeStyles((theme) => ({
-    resumeWork: {
-        padding: '10px 0',
-        borderBottom: '1px solid #ddd',
-    },
-    works: {
-        margin: '0',
-        padding: '0',
-        listStyle: 'none',
-        '& li': { margin: '0 0 10px 0', '&:last-child': { margin: '0' } },
-    },
-    position: { fontWeight: 'bold' },
-    website: {},
-    summary: {},
-    highlights: {
-        listStyle: 'disc',
-        '& li': { fontStyle: 'italic', margin: '0' },
-    },
-}));
+const useStyles = makeStyles((theme) => {
+    return {
+        resumeWork: {
+            padding: '10px 0',
+            borderBottom: '1px solid #ddd',
+        },
+        works: {
+            margin: '0',
+            padding: '0',
+            listStyle: 'none',
+            '& li': { margin: '0 0 10px 0', '&:last-child': { margin: '0' } },
+        },
+        position: { fontWeight: 'bold' },
+        positionDate: {
+            fontStyle: 'italic',
+            fontSize: '0.8rem',
+        },
+        website: {},
+        summary: {
+            whiteSpace: 'break-spaces',
+        },
+        highlights: {
+            listStyle: 'none',
+            paddingLeft: 0,
+            display: 'inline-flex',
+            '& li': {
+                fontStyle: 'italic',
+                margin: '0 3px 0 0',
+                backgroundColor: theme.palette.type === 'dark' ? '#28407b' : '#dae4f4',
+                borderRadius: '3px',
+                padding: '1px 3px',
+            },
+        },
+    };
+});
 
 const Work = ({ work: works }) => {
     const classes = useStyles();
@@ -50,18 +66,31 @@ const Work = ({ work: works }) => {
                         return (
                             <li key={uuid()}>
                                 <p className={classes.position}>
-                                    {position?.enabled && `${position?.value}, `}
-                                    {company?.enabled && `${company?.value}, `}
-                                    {name?.enabled && `${name?.value}, `}
-                                    {startDate?.enabled && startDate?.value}
-                                    {' - '}
-                                    {endDate?.enabled && endDate?.value}
+                                    {position?.enabled && `${position?.value} @ `}
+                                    {company?.enabled && company?.value}
+                                    {name?.enabled && name?.value}
+                                    {(startDate?.enabled || endDate?.enabled) && (
+                                        <span className={classes.positionDate}>
+                                            {' ('}
+                                            {startDate?.enabled && endDate?.value}
+                                            {endDate?.enabled && ` - ${endDate?.value}`}
+                                            {')'}
+                                        </span>
+                                    )}
                                 </p>
                                 <p className={classes.website}>
-                                    {website?.enabled && website?.value}
+                                    {website?.enabled && (
+                                        <a href={website?.value}>
+                                            {website?.value}
+                                        </a>
+                                    )}
                                 </p>
                                 <p className={classes.website}>
-                                    {url?.enabled && url?.value}
+                                    {url?.enabled && (
+                                        <a href={url?.value}>
+                                            {url?.value}
+                                        </a>
+                                    )}
                                 </p>
                                 <p className={classes.summary}>
                                     {summary?.enabled && summary?.value}
