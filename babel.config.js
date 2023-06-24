c65dac3aa7620c466caf9cff2d617db06b78e149
Pablo Pirata <pablo@studocu.com>
2023-06-24 18:03:08 +0200

    fix build

--- babel.config.js
+++ babel.config.js
@@ -10,12 +10,5 @@ module.exports = {
             },
         ],
     ],
-    plugins: [
-        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-        ['@babel/plugin-proposal-optional-chaining', { loose: false }],
-        ['@babel/plugin-proposal-function-bind', { loose: false }],
-        ['@babel/plugin-proposal-private-methods', { loose: false }],
-        ['@babel/plugin-transform-async-to-generator', { loose: false }],
-    ],
+    plugins: [],
 };
